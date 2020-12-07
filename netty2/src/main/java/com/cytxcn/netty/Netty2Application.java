0df710359acdd5befeb32f6228e2c1d2866a883c
Administrator <Administrator@PC201912031613>
2020-12-07 17:15:44 +0800

    解决netty启动  阻塞springboot

--- netty2/src/main/java/com/cytxcn/netty/Netty2Application.java
+++ netty2/src/main/java/com/cytxcn/netty/Netty2Application.java
@@ -1,15 +1,30 @@
 package com.cytxcn.netty;
 
+import org.springframework.beans.factory.annotation.Autowired; 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+
+import com.cytxcn.netty.server.ProtocolServer;
 
 @SpringBootApplication
-public class Netty2Application {
+public class Netty2Application implements CommandLineRunner {
+
+	@Autowired
+	private ProtocolServer nettyServer;
 
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Netty2Application.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		nettyServer.run();
+	}
+	
+	
+	
+	
 
 }
